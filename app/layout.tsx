b8e6b850faf358da0e0a80115ab7cfd733b1b1b8
Nizar Sarieldin <nzrsrd@Mac.fritz.box>
2026-07-22 22:20:33 +0200

    adjust the color

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark:bg-black dark:text-zinc-400`}
     >
       <body>
         <Navbar />
